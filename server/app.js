8b89bf810ca8a74edf3c2f18071617e8eee46ad3
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-07-02 16:13:55 +0900

    app.js
    app.js 원상복구

--- server/app.js
+++ server/app.js
@@ -9,8 +9,16 @@ app.listen(process.env.PORT);
 
 const userRouter = require('./routes/users');
 const schedulesRouter = require('./routes/schedules');
+<<<<<<< HEAD
 const todolistRouter = require('./routes/todolist');
 
 app.use("/users",userRouter);
 app.use("/schedules",schedulesRouter);
 app.use("/todolist",todolistRouter);
+=======
+const todoRouter = require('./routes/todos');
+
+app.use("/users",userRouter);
+app.use("/schedules",schedulesRouter);
+app.use("/todos",todoRouter);
+>>>>>>> a8e4540 (app.js)
