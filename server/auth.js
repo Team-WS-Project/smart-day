91af8ea3bdde595c8fa6ef4a2a7a2857382a9522
Lmh98 <1004alsgud@gmail.com>
2024-07-02 23:42:46 +0900

    콘솔 출력 주석처리

--- server/auth.js
+++ server/auth.js
@@ -1,27 +1,25 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const jwt = require("jsonwebtoken");
 
-
 const ensureAuthorization = (req, res) => {
+  try {
+    let receivedJwt = req.headers["authorization"];
+    // console.log("recieved jwt : ", receivedJwt);
 
-    try {
-        let receivedJwt = req.headers["authorization"];
-        console.log("recieved jwt : ", receivedJwt);
-
-        if (receivedJwt) {
-            let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-            // console.log(decodedJwt);
-            return decodedJwt;
-        } else {
-            throw new ReferenceError("jwt must be provided");
-        }
-    } catch (err) {
-        console.log(err.name);
-        console.log(err.message);
-
-        return err;
+    if (receivedJwt) {
+      let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+      // console.log(decodedJwt);
+      return decodedJwt;
+    } else {
+      throw new ReferenceError("jwt must be provided");
     }
-}
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+
+    return err;
+  }
+};
 
 module.exports = ensureAuthorization;
