33fedbb6ed947e1db28d4b4ad0133ef4535b6132
Lmh98 <1004alsgud@gmail.com>
2024-07-04 01:38:38 +0900

    주석 수정

--- server/auth.js
+++ server/auth.js
@@ -5,19 +5,14 @@ const jwt = require("jsonwebtoken");
 const ensureAuthorization = (req, res) => {
   try {
     let receivedJwt = req.headers["authorization"];
-    // console.log("recieved jwt : ", receivedJwt);
 
     if (receivedJwt) {
       let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-      // console.log(decodedJwt);
       return decodedJwt;
     } else {
       throw new ReferenceError("jwt must be provided");
     }
   } catch (err) {
-    // console.log(err.name);
-    // console.log(err.message);
-
     return err;
   }
 };
