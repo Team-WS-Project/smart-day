2681bb3cac5f37187758c2c90d4decd8483f91c1
Lmh98 <1004alsgud@gmail.com>
2024-07-04 01:49:46 +0900

    토큰 유효성 auth.js에서 검사

--- server/auth.js
+++ server/auth.js
@@ -1,19 +1,36 @@
 const dotenv = require("dotenv");
-dotenv.config();
 const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 
-const ensureAuthorization = (req, res) => {
+dotenv.config();
+
+const ensureAuthorization = (req, res, next) => {
   try {
     let receivedJwt = req.headers["authorization"];
 
-    if (receivedJwt) {
-      let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-      return decodedJwt;
-    } else {
+    if (!receivedJwt) {
       throw new ReferenceError("jwt must be provided");
     }
+
+    let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+    req.authorization = decodedJwt; // 요청 객체에 디코딩된 정보 저장
+    next(); // 다음 미들웨어 호출
   } catch (err) {
-    return err;
+    if (err instanceof jwt.TokenExpiredError) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "로그인 세션이 만료되었습니다. 다시 로그인 하세요.",
+      });
+    } else if (err instanceof jwt.JsonWebTokenError) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "잘못된 토큰입니다.",
+        error: err.message, // 추가적인 에러 메시지 반환
+      });
+    } else {
+      console.error(err);
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: "서버 오류입니다. 다시 시도해 주세요.",
+      });
+    }
   }
 };
 
