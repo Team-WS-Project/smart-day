d9a0742e2d70518a0fc9ca6372085a893dd49931
Lmh98 <1004alsgud@gmail.com>
2024-07-04 01:15:19 +0900

    주석 수정

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,8 +13,8 @@ router.use(express.json());
 
 router.get("/", getUserInformation); // 메인페이지 location, nickname 조회
 router.get("/update", getInfoToUpdate); // 회원 정보 수정창 email, nickname, location 조회
-router.post("/join", join); // 회원가입
-router.post("/login", login); // 로그인
+router.post("/join", join);
+router.post("/login", login);
 router.put("/update", updateUserInformation); // 회원 정보 수정
 
 module.exports = router;
