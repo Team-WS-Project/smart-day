321bdbf6334aaa6d3328fdfec43c6b247d238f32
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-07-03 21:26:00 +0900

    pr review second modify
    main branch rebase하다가 충돌발생해서 다시 브랜치 생성 후 내용 수정했습니다..리뷰의견 많이 반영해서 적용했습니다.

---
+++ server/routes/todos.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  addTodos,
+  getTodos,
+  getTodoDetail,
+  editTodos,
+  deleteTodos,
+  changeCompleted,
+} = require("../controller/todosController");
+
+const router = express.Router();
+router.use(express.json());
+
+router.post("/", addTodos);
+router.get("/", getTodos);
+router.get("/:id", getTodoDetail);
+router.put("/:id", editTodos);
+router.put("/completed/:id", changeCompleted);
+router.delete("/:id", deleteTodos);
+
+module.exports = router;
