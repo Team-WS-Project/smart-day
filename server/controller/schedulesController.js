db0bc747177cfb6e3a1e1649a70e6f423d83ebfa
una3325 <a33256448@gmail.com>
2024-07-08 06:18:45 +0900

    0708 - edit getAllSchedules

--- server/controller/schedulesController.js
+++ server/controller/schedulesController.js
@@ -1,14 +1,22 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
+//일정 조회
 const getAllSchedules = (req, res) => {
-  const { start, end } = req.query;
+  const { start, end, date, month } = req.query;
+
   let values = [];
   let sql = "SELECT * FROM SmartDay.schedules";
 
   if (start && end) {
     sql += " WHERE date BETWEEN ? AND ?";
     values = [start, end];
+  } else if (date) {
+    sql += " WHERE date = ?";
+    values = [date];
+  } else if (month) {
+    sql += " WHERE DATE_FORMAT(date, '%Y-%m') = ?";
+    values = [month];
   }
 
   conn.query(sql, values, (err, results) => {
@@ -20,36 +28,14 @@ const getAllSchedules = (req, res) => {
   });
 };
 
-// 날짜별 일정 조회 
-///ex. schedules/date/2024-06-01
-const getSchedulesByDate = (req, res) => {
-  const { date } = req.params;
-  let sql = "SELECT * FROM schedules WHERE date = ?";
-  let values = [date];
-
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.error(err);
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-    }
-
-    if (results.length === 0) {
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ message: "해당 날짜의 일정이 없습니다." });
-    }
-
-    res.status(StatusCodes.OK).json(results);
-  });
-};
-
-//개별 일정 조회
+// 개별 일정 조회
 const getScheduleById = (req, res) => {
   const { id } = req.params;
+
   let sql = `
-      SELECT date, start_time, end_time, title, detail, completed
-      FROM schedules
-      WHERE id = ?
+    SELECT title, date, start_time, end_time, detail, completed
+	  FROM schedules
+	  WHERE id = ?
     `;
   let values = [id];
 
@@ -73,7 +59,7 @@ const getScheduleById = (req, res) => {
 const addSchedule = (req, res) => {
   const { title, date, startTime, endTime } = req.body;
   let sql =
-    "INSERT INTO schedules (Title, date, start_time, end_time) VALUES(?, ?, ?, ?)";
+    "INSERT INTO schedules (title, date, start_time, end_time) VALUES (?, ?, ?, ?)";
   let values = [title, date, startTime, endTime];
 
   conn.query(sql, values, (err, results) => {
@@ -86,36 +72,34 @@ const addSchedule = (req, res) => {
   });
 };
 
-//개별 일정 수정
+// 개별 일정 수정
 const updateSchedule = (req, res) => {
   const { id } = req.params;
-  const { title, date, startTime, endTime, Detail, completed } = req.body;
+  const { title, date, startTime, endTime, detail, completed } = req.body;
 
   let sql = `
         UPDATE schedules
         SET title = ?, date = ?, start_time = ?, end_time = ?, detail = ?, completed = ?
         WHERE id = ?
       `;
-  let values = [title, date, startTime, endTime, Detail, completed, id];
+  let values = [title, date, startTime, endTime, detail, completed, id];
 
-  conn.query(sql, values, (err, result) => {
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.error(err);
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
     }
 
-    res.status(StatusCodes.OK).json({
-      message: "일정 수정 놘료",
-    });
+    res.status(StatusCodes.OK).json(results);
   });
 };
 
-//개별 일정 삭제
+// 개별 일정 삭제
 const deleteSchedule = (req, res) => {
   const { id } = req.params;
   let sql = "DELETE FROM schedules WHERE id = ?";
 
-  conn.query(sql, id, (err, result) => {
+  conn.query(sql, id, (err, results) => {
     if (err) {
       console.error(err);
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
@@ -124,16 +108,15 @@ const deleteSchedule = (req, res) => {
     if (result.affectedRows === 0) {
       return res
         .status(StatusCodes.NOT_FOUND)
-        .json({ message: "일정을 찾을 수 없습니다." });
+        .json({ message: "해당 일정을 조회할 수 없습니다." });
     }
 
-    res.status(StatusCodes.OK).json({ message: "일정 삭제 완료" });
+    res.status(StatusCodes.OK).json(results);
   });
 };
 
 module.exports = {
   getAllSchedules,
-  getSchedulesByDate,
   getScheduleById,
   addSchedule,
   updateSchedule,
