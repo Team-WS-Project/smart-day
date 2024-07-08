1fe8b32c00e382514f34964157073a47f3192ba3
leedohyung28 <dhleee0123@gmail.com>
2024-07-08 22:13:57 +0900

    0708 frontend merge & folder 정리

--- client/src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-// store.js
-import { configureStore } from "@reduxjs/toolkit";
-import locationReducer from "./locationSlice";
-
-export const store = configureStore({
-  reducer: {
-    location: locationReducer,
-  },
-});
