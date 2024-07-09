e4495ea47502dbed9b959ad9e137c996eba21cf9
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-07-09 16:54:34 +0900

    0709 sql 함수로 빼놓기

--- server/controller/todosController.js
+++ server/controller/todosController.js
@@ -1,23 +1,46 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
+//TODO: SQL로 테이블과 연결되는 부분은 나중에 파일 분리할 예정 (@ hyoeun0001)
+function getTodayTodos({ userId, today, month }) {
+  const sql = "SELECT id, due_date, title, completed FROM todos WHERE user_id = ? AND due_date > ?";
+  const values = [userId, today];
+
+  return [sql, values];
+}
+
+function getMonthTodos({ userId, today, month }) {
+  const sql = "SELECT id, due_date, title, completed FROM todos WHERE user_id = ? AND due_date LIKE ?";
+  const values = [userId, month + "%"];
+
+  return [sql, values];
+}
+
+function getDefaultTodos({ userId, today, month }) {
+  const sql = `SELECT id, due_date, title, completed FROM todos WHERE user_id = ? `;
+  const values = [userId];
+
+  return [sql, values];
+}
+
+function getTodosQuery({ userId, today, month }) {
+  if (today) {
+    return getTodayTodos({ userId, today, month });
+  }
+
+  if (month) {
+    return getMonthTodos({ userId, today, month });
+  }
+
+  return getDefaultTodos({ userId, today, month });
+}
+
 const getTodos = (req, res) => {
   // TODO: user_id는 users랑 합칠때 authorization.id로 변경될 것 (@ hyoeun0001)
   const { userId } = req.body;
   const { today, month } = req.query;
 
-  let sql = `SELECT id, due_date, title, completed FROM todos WHERE user_id = ? `;
-  let values = [];
-
-  if (today) {
-    values.push(userId, today);
-    sql += `AND due_date >= ?`;
-  } else if (month) {
-    values.push(userId, month + "%");
-    sql += `AND due_date LIKE ?`;
-  } else {
-    values = [userId];
-  }
+  const [sql, values] = getTodosQuery({ userId, today, month });
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -49,17 +72,33 @@ const getTodoDetail = (req, res) => {
   });
 };
 
+function addDefaultTodos({ title, detail, dueDate, userId }) {
+  const sql = `INSERT INTO todos (title, detail, due_date, user_id) VALUES(?, ?, ?, ?)`;
+  const values = [title, detail, dueDate, userId];
+
+  return [sql, values];
+}
+
+function addTodosAsDetailNull({ title, detail, dueDate, userId }) {
+  const sql = `INSERT INTO todos (title, due_date, user_id) VALUES(?, ?, ?)`;
+  const values = [title, dueDate, userId];
+
+  return [sql, values];
+}
+
+function addTodosQuery({ title, detail, dueDate, userId }) {
+  if (detail == undefined) {
+    return addTodosAsDetailNull({ title, detail, dueDate, userId });
+  }
+
+  return addDefaultTodos({ title, detail, dueDate, userId });
+}
+
 const addTodos = (req, res) => {
   // TODO: userId는 users랑 합칠때 authorization.id로 변경될 것 (@ hyoeun0001)
   const { title, detail, dueDate, userId } = req.body;
 
-  let sql = `INSERT INTO todos (title, detail, due_date, user_id) VALUES(?, ?, ?, ?)`;
-  let values = [title, detail, dueDate, userId];
-
-  if (detail === undefined) {
-    sql = `INSERT INTO todos (title, due_date, user_id) VALUES(?, ?, ?)`;
-    values = [title, dueDate, userId];
-  }
+  const [sql, values] = addTodosQuery({ title, detail, dueDate, userId });
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -74,17 +113,33 @@ const addTodos = (req, res) => {
   });
 };
 
+function updateDefaultTodos({ id, title, detail, dueDate }) {
+  const sql = `UPDATE todos SET title = ?, detail = ?, due_date = ? WHERE id = ?`;
+  const values = [title, detail, dueDate, id];
+
+  return [sql, values];
+}
+
+function updateTodosAsDetailNull({ id, title, detail, dueDate }) {
+  const sql = `UPDATE todos SET title = ?, due_date = ? WHERE id = ?`;
+  const values = [title, dueDate, id];
+
+  return [sql, values];
+}
+
+function updateTodosQuery({ id, title, detail, dueDate }) {
+  if (detail == undefined) {
+    return updateTodosAsDetailNull({ id, title, detail, dueDate });
+  }
+
+  return updateDefaultTodos({ id, title, detail, dueDate });
+}
+
 const updateTodos = (req, res) => {
   const { id } = req.params;
   const { title, detail, dueDate } = req.body;
 
-  let sql = `UPDATE todos SET title = ?, detail = ?, due_date = ? WHERE id = ?`;
-  let values = [title, detail, dueDate, id];
-
-  if (detail === undefined) {
-    sql = `UPDATE todos SET title = ?, due_date = ? WHERE id = ?`;
-    values = [title, dueDate, id];
-  }
+  const [sql, values] = updateTodosQuery({ id, title, detail, dueDate });
 
   conn.query(sql, values, (err, results) => {
     if (err) {
