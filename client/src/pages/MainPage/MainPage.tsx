748cdf70a93972ef037e06ae304edd82ec67e627
II-122 <76612998+II-122@users.noreply.github.com>
2024-07-13 03:44:28 +0900

    0713 mainStore commit-01
    2024-07-13 메인 페이지 mainStore update-01

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -10,30 +10,51 @@ import { datePick,
   weatherText
 } from "./MainPage.css";
 import { appContainer } from "../../App.css";
+import DatepickerModal from "../../components/ModalComponents/DatepickerModal/DatepickerModal";
+import TaskModal from "../../components/ModalComponents/TaskModal/TaskModal";
+import LoginModal from "../../components/ModalComponents/LoginModal/LoginModal";
+import UserEditModal from "../../components/ModalComponents/UserEditModal/UserEditModal";
+import useModalStore, { toggleDatepickerModal } from "../../store/store";
 
 const MainPage = () => {
   const navigate = useNavigate();
-
   const gotoCalendarPage = () => {
     navigate("/calendar");
   };
 
+  const { loginModal, userEditModal, datepickerModal, taskModal, } = useModalStore();
+
   return (
     <div className={appContainer}>
       <Header />
+      {
+        loginModal ? <LoginModal /> : null
+      }
+      {
+        userEditModal ? <UserEditModal /> : null
+      }
+      {
+        datepickerModal ? <DatepickerModal /> : null
+      }
+      {
+        taskModal ? <TaskModal /> : null
+      }
       <div className={pageContainer}>
         <div className={locationTextArea}>현재 지역은 location 입니다.</div>
         <div>
           <ListContainer />
         </div>
         <div className={textArea}>
-          <div className={datePick}>
+          <div
+            className={datePick}
+            onClick={toggleDatepickerModal}
+            >
             + 월별 일정 추가
           </div>
           <div className={weatherText}>오늘은 날씨가 weather이므로, textObj 하시는건 어때요?</div>
           <div
             className={linkCalendarPage}
-            onClick={gotoCalendarPage} //
+            onClick={gotoCalendarPage}
           >
             전체 달력 보기
           </div>
