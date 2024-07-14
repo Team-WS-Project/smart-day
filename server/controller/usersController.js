2e9347e8752bf504af0102b5c34c9b2bab98b599
Lmh98 <1004alsgud@gmail.com>
2024-07-15 00:07:02 +0900

    오타 및 서식 수정

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,33 +1,11 @@
 const dotenv = require("dotenv");
-dotenv.config();
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const ensureAuthorization = require("../auth");
 
-// 메인페이지 location, nickname 조회
-// const getUser = (req, res) => {
-//   ensureAuthorization(req, res, () => {
-//     // 인증 성공 시 실행될 코드
-//     const sql = `SELECT location, nickname FROM users WHERE email = ?`;
-
-//     conn.query(sql, req.authorization.email, (err, results) => {
-//       if (err) {
-//         console.error(err);
-//         return res.status(StatusCodes.BAD_REQUEST).end();
-//       }
-
-//       if (results.length === 0) {
-//         return res.status(StatusCodes.NOT_FOUND).json({
-//           message: "사용자 정보를 찾을 수 없습니다.",
-//         });
-//       }
-
-//       return res.status(StatusCodes.OK).json(results[0]);
-//     });
-//   });
-// };
+dotenv.config();
 
 // 회원 정보 수정창 email, nickname, location 조회, 메인페이지 nickname, location 조회
 const getUser = (req, res) => {
@@ -58,9 +36,9 @@ const join = (req, res) => {
   const sql = `INSERT INTO users (email, password, salt, location, nickname) VALUES (?, ?, ?, ?, ?)`;
 
   const salt = crypto.randomBytes(16).toString("base64");
-  const hashPassword = hashPassword(password, salt);
+  const hashedPassword = hashPassword(password, salt);
 
-  const values = [email, hashPassword, salt, location, nickname];
+  const values = [email, hashedPassword, salt, location, nickname];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -99,10 +77,10 @@ const login = (req, res) => {
     const loginUser = results[0];
 
     // salt값 꺼내서 날 것으로 들어온 비밀번호 암호화
-    const hashPassword = hashPassword(password, loginUser.salt);
+    const hashedPassword = hashPassword(password, loginUser.salt);
 
     // 위에서 암호화된 비밀번호 db 비밀번호랑 비교
-    if (loginUser && loginUser.password == hashPassword) {
+    if (loginUser && loginUser.password == hashedPassword) {
       const access_token = jwt.sign(
         {
           id: loginUser.id,
