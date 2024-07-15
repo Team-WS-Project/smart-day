4af762fcbc9d07e93ac55a34a8cf462764302dd3
JS LEE <109492059+JSLEE753@users.noreply.github.com>
2024-07-16 03:26:40 +0900

    TodolistPage 기능 구현
    변경사항 :
    1. 체크박스 클릭시, 각 리스트에 맞게 리렌더링
    2. todo의 description 부분 클릭 시 , todo 모달 출력
    
    구현해야할 사항 :
    1. 임시데이터 대신 실제 데이터 fetch
    2. global state에 todolistpage에서 클릭한 todo의 id를 저장해야함.
    이 id를 통해 서버에 id에 맞는 todo 정보들을 요청해야함.

---
+++ client/src/store/store.tsx
@@ -0,0 +1,156 @@
+import { create, StateCreator } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type ModalType =
+  | "datepickerModal"
+  | "dayModal"
+  | "locationModal"
+  | "loggerModal"
+  | "loginModal"
+  | "registerModal"
+  | "taskModal"
+  | "todoScheduleModal"
+  | "userEditModal";
+interface ModalState {
+  datepickerModal: boolean;
+  dayModal: boolean;
+  locationModal: boolean;
+  loggerModal: boolean;
+  loginModal: boolean;
+  registerModal: boolean;
+  taskModal: boolean;
+  todoScheduleModal: boolean;
+  selectedTodoScheduleId: number | null;
+  userEditModal: boolean;
+
+  actions: {
+    changeModalState: (type: ModalType) => void;
+  };
+}
+
+// type todo = {
+//   todoId: number;
+//   isChecked: boolean;
+//   dueDate: Date;
+// };
+
+// type todos = {
+//   failureTodos: todo[];
+//   completedTodos: todo[];
+//   todayTodos: todo[];
+// };
+
+interface userInfoState {
+  userId: number | null;
+  selectedDate: Date | undefined;
+  currentLocation: string;
+  favoriteLocations: string[];
+  // todos: todos;
+
+  actions: {
+    // 1. 선택된 날짜를 저장하는 리듀서
+    setSelectedDate: (arg1: Date) => void;
+
+    // 2. 선택된 날짜를 초기화하는 리듀서
+    initializeSelectedDate: () => void;
+
+    // 3. 현재 위치를 선택하는 리듀서
+    setCurrentLocation: (currentLocation: string) => void;
+
+    // 4. 현재 위치를 즐겨찾기하는 리듀서
+    addFavoriteLocation: (location: string) => void;
+
+    // 5. 즐겨찾기한 위치를 삭제하는 리듀서
+    deleteFavoriteLocation: (location: string) => void;
+  };
+}
+
+const modalStore: StateCreator<ModalState> = (set) => ({
+  datepickerModal: false,
+  dayModal: false,
+  locationModal: false,
+  loggerModal: false,
+  loginModal: false,
+  registerModal: false,
+  taskModal: false,
+  todoScheduleModal: false,
+  selectedTodoScheduleId: null,
+  userEditModal: false,
+  actions: {
+    changeModalState: (type) => {
+      set((state) => ({ ...state, [type]: !state[type] }));
+    },
+  },
+});
+
+const userInfoStore: StateCreator<userInfoState> = (set) => ({
+  userId: null,
+  selectedDate: undefined,
+  currentLocation: "",
+  favoriteLocations: [],
+
+  actions: {
+    setSelectedDate: (date) => {
+      set((state) => ({ ...state, selectedDate: date }));
+    },
+
+    initializeSelectedDate: () => {
+      set((state) => ({ ...state, selectedDate: undefined }));
+    },
+
+    setCurrentLocation: (currentLocation) => {
+      set((state) => ({ ...state, currentLocation: currentLocation }));
+    },
+
+    addFavoriteLocation: (location) => {
+      set((state) => {
+        if (location.trim() === "") {
+          return { ...state };
+        }
+
+        let isOverlapped = false;
+
+        state.favoriteLocations.forEach((favoriteLocation) => {
+          if (favoriteLocation === location) {
+            isOverlapped = true;
+          }
+        });
+
+        if (isOverlapped) {
+          return { ...state };
+        }
+
+        return {
+          ...state,
+          favoriteLocations: [...state.favoriteLocations, location],
+        };
+      });
+    },
+
+    deleteFavoriteLocation: (location) => {
+      set((state) => {
+        return {
+          ...state,
+          favoriteLocations: state.favoriteLocations.filter((favoriteLocation) => {
+            return favoriteLocation !== location;
+          }),
+        };
+      });
+    },
+  },
+});
+
+const useModalStore = create<ModalState>()(devtools(modalStore, { name: "Modal Store" }));
+export const useUserInfoStore = create<userInfoState>()(devtools(userInfoStore, { name: "User Info Store" }));
+
+export const toggleDatepickerModal = () => useModalStore.getState().actions.changeModalState("datepickerModal");
+export const toggleDayModal = () => useModalStore.getState().actions.changeModalState("dayModal");
+export const toggleLocationModal = () => useModalStore.getState().actions.changeModalState("locationModal");
+export const toggleLoggerModal = () => useModalStore.getState().actions.changeModalState("loggerModal");
+export const toggleLoginModal = () => useModalStore.getState().actions.changeModalState("loginModal");
+export const toggleRegisterModal = () => useModalStore.getState().actions.changeModalState("registerModal");
+export const toggleTaskModal = () => useModalStore.getState().actions.changeModalState("taskModal");
+export const toggleTodoScheduleModal = () => useModalStore.getState().actions.changeModalState("todoScheduleModal");
+export const toggleUserEditModal = () => useModalStore.getState().actions.changeModalState("userEditModal");
+
+export default useModalStore;
