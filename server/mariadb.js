a63722ea7a1b524be82eb1f34bdca7fea3f660c6
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-07-18 16:42:42 +0900

    0718 배포된 db로 변경

--- server/mariadb.js
+++ server/mariadb.js
@@ -4,8 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mariadb.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.HOST_NAME,
+  port: process.env.MARIADB_PORT,
+  user: process.env.MARIADB_ROOT,
   password: process.env.MARIADB_PASSWORD,
   database: process.env.DATABASE,
   dateStrings: true,
