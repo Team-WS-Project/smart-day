37662cbd7b60474865250b8f75d7bb13b1c0a5c4
Lmh98 <1004alsgud@gmail.com>
2024-07-23 21:42:28 +0900

    로그아웃 api 추가

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb");
-const {
-  getUser,
-  join,
-  login,
-  updateUserInformation,
-} = require("../controller/usersController");
+const { getUser, join, login, updateUserInformation, logout } = require("../controller/usersController");
 
 router.use(express.json());
 
 router.get("/", getUser); // 회원 정보 수정창 email, nickname, location 조회, 메인페이지 location, nickname 조회
 router.post("/join", join);
 router.post("/login", login);
+router.post("/logout", logout);
 router.put("/update", updateUserInformation); // 회원 정보 수정
 
 module.exports = router;
