5be4b5700653f9255d904f11cca0d6ad16690777
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-07-23 16:06:49 +0900

    app.js에 있는 프론트 url env로 수정

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
