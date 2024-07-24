5c95ec26bf9c277ad87d9ad874062349165310a4
leedohyung28 <dhleee0123@gmail.com>
2024-07-24 21:57:07 +0900

    0724 회원 정보 수정

--- client/src/apis/loginAPI.ts
+++ client/src/apis/loginAPI.ts
@@ -5,6 +5,6 @@ export const requestLoginAPI = async (email: string, password: string) => {
     const res = await baseAxios.post("/users/login", { email: email, password: password });
     return res;
   } catch (err) {
-    alert("이메일 또는 비밀번호를 확인해주세요.");
+    console.error(err);
   }
 };
--- client/src/components/ModalComponents/RegisterEditModal/RegisterEditModal.tsx
+++ client/src/components/ModalComponents/RegisterEditModal/RegisterEditModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   inputField,
   inputBox,
@@ -11,65 +11,31 @@ import {
   wrapper,
 } from "./RegisterEditModal.css";
 import { VscChromeClose } from "react-icons/vsc";
-import { CiMail } from "react-icons/ci";
-import { IoLockClosedOutline } from "react-icons/io5";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { GoTag } from "react-icons/go";
 import { toggleLocationModal, toggleLoginModal, toggleUserEditModal } from "../../../store/modalStore";
-import { requestRegisterAPI } from "../../../apis/registerAPI";
 import { useChangeUserInfoStore } from "../../../store/changeUserInfoStore";
 import { useUserInfoStore } from "../../../store/userInfoStore";
+import { editUserInfoAPI } from "../../../apis/editUserInfoAPI";
 
 const RegisterModal = () => {
-  const { email, nickname } = useChangeUserInfoStore((state) => ({
-    email: state.email,
+  const { nickname } = useChangeUserInfoStore((state) => ({
     nickname: state.nickname,
   }));
-  const [type, setType] = useState("password");
-  const [showPassword, setShowPassword] = useState(false);
-  const [icon, setIcon] = useState(<FaEyeSlash />);
 
-  const [newEmail, setEmail] = useState(email || "");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [newNickName, setNickname] = useState(nickname || "");
 
   const { currentLocation } = useUserInfoStore();
   const location = currentLocation;
 
-  const [typeConfirmPassword, setTypeConfirmPassword] = useState("password");
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [iconConfirmPassword, setIconConfirmPassword] = useState(<FaEyeSlash />);
+  const handleRegister = async (location: string, newNickName: string) => {
+    try {
+      await editUserInfoAPI(location, newNickName);
 
-  const handleToggle = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
-    setType((prevType) => (prevType === "password" ? "text" : "password"));
-    setIcon((prevIcon) => (prevIcon.type === FaEyeSlash ? <FaEye /> : <FaEyeSlash />));
-  };
-
-  const handleToggleConfirmPassword = () => {
-    setShowConfirmPassword((prevShowConfirmPassword) => !prevShowConfirmPassword);
-    setTypeConfirmPassword((prevType) => (prevType === "password" ? "text" : "password"));
-    setIconConfirmPassword((prevIcon) => (prevIcon.type === FaEyeSlash ? <FaEye /> : <FaEyeSlash />));
-  };
-
-  const handleRegister = async (newEmail: string, password: string, location: string, newNickName: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(newEmail)) {
-      alert("올바른 이메일 형식을 입력해주세요.");
-    } else if (password !== confirmPassword) {
-      alert("비밀번호가 일치하지 않습니다.");
-    } else {
-      try {
-        await requestRegisterAPI(newEmail, password, location, newNickName);
-
-        alert("회원정보를 수정했습니다! 로그인을 진행해주세요.");
-        toggleUserEditModal();
-        toggleLoginModal();
-      } catch (error) {
-        alert("회원정보 수정에 실패했습니다.");
-      }
+      alert("회원정보를 수정했습니다! 로그인을 진행해주세요.");
+      toggleUserEditModal();
+      toggleLoginModal();
+    } catch (error) {
+      alert("회원정보 수정에 실패했습니다.");
     }
   };
 
@@ -80,49 +46,6 @@ const RegisterModal = () => {
           <VscChromeClose />
         </div>
         <div className={loginMain}>SMART DAY</div>
-
-        <div className={inputField}>
-          <div className={inputIcon}>
-            <CiMail />
-          </div>
-
-          <input
-            className={inputBox}
-            placeholder="아이디 (email)"
-            value={newEmail}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className={inputField}>
-          <div className={inputIcon}>
-            <IoLockClosedOutline />
-          </div>
-          <input
-            className={inputBox}
-            placeholder="비밀번호"
-            type={type}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div className={inputIcon} onClick={handleToggle} style={{ cursor: "pointer" }}>
-            {icon}
-          </div>
-        </div>
-        <div className={inputField}>
-          <div className={inputIcon}>
-            <IoLockClosedOutline />
-          </div>
-          <input
-            className={inputBox}
-            placeholder="비밀번호 확인"
-            type={typeConfirmPassword}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <div className={inputIcon} onClick={handleToggleConfirmPassword}>
-            {iconConfirmPassword}
-          </div>
-        </div>
         <div className={inputField}>
           <div className={inputIcon}>
             <GoTag />
@@ -137,7 +60,7 @@ const RegisterModal = () => {
         <div className={locationButton} onClick={toggleLocationModal}>
           위치 설정
         </div>
-        <div className={registerButton} onClick={() => handleRegister(newEmail, password, location, newNickName)}>
+        <div className={registerButton} onClick={() => handleRegister(location, newNickName)}>
           회원정보 수정
         </div>
       </div>
